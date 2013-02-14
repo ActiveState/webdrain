4fc00e32403675c4cdd28a954cf190cafa3bc098
Sridhar Ratnakumar <sridharr@activestate.com>
2013-02-14 12:43:01 -0800

    redis integration

--- app.js
+++ app.js
@@ -1,8 +1,25 @@
 var app = require('express')()
   , server = require('http').createServer(app)
-  , io = require('socket.io').listen(server);
+  , io = require('socket.io').listen(server)
+  , redis = require('redis')
+  , url = require('url');
 
-var PORT = process.env.PORT || 8080;
+var PORT = process.env.PORT || 8080
+var REDIS_URL = url.parse(
+  process.env.REDIS_URL || "redis://localhost:6379")
+
+function newRedisClient() {
+  var redisCli = redis.createClient(REDIS_URL.port, REDIS_URL.hostname)
+  console.log(REDIS_URL)
+  if (REDIS_URL.auth != undefined) {
+    var pass = REDIS_URL.auth.split(":")[1];
+    redisCli.auth(pass)
+  }
+  return redisCli;
+}
+
+var redisMain = newRedisClient();
+
 server.listen(PORT)
 
 app.get('/', function (req, res) {
@@ -10,10 +27,20 @@ app.get('/', function (req, res) {
 });
 
 io.sockets.on('connection', function (socket) {
-  socket.emit('news', { hello: 'world' });
-  socket.on('my other event', function (data) {
-    console.log(data);
-  });
+  socket.emit('messages', 'hello world');
+
+  console.log("redis: init")
+  var redisConn = newRedisClient();
+  redisConn.on("message", function(channel, message) {
+    console.log("redis message " + channel + ": " + message)
+    socket.emit('messages', message)
+  })
+  redisConn.on("subscribe", function(channel, count) {
+    console.log("redis: publishing on main")
+    redisMain.publish("drain", "sent via redis")
+  })
+  console.log("redis: subscribing..")
+  redisConn.subscribe("drain")
 });
 
 console.log("Running at http://localhost:" + PORT)
