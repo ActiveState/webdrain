990ec9a0596b30bf65af006b12637397f7f8454f
Sridhar Ratnakumar <sridharr@activestate.com>
2013-02-14 13:06:42 -0800

    bind to harbor; pub to websocket clients using redis

--- README.md
+++ README.md
@@ -1,4 +1,21 @@
 webdrain
 ========
 
-Logyard drain as a web app
+Logyard drain as a web app.
+
+```
+$ stackato push -n
+...
+$ stackato service webdrain-tcp
+...
+```
+
+Note down the port from that last command. And then setup a drain
+like:
+
+```
+$ kato log drain add webdrain tcp://stackato-xyza.local:12345/
+```
+
+Watch the log messages come in.
+
--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 var app = require('express')()
+  , net = require('net')
+  , readline = require('readline')
   , server = require('http').createServer(app)
   , io = require('socket.io').listen(server)
   , redis = require('redis')
   , url = require('url');
 
 var PORT = process.env.PORT || 8080
+var TCP_PORT = process.env.STACKATO_HARBOR || 8081
 var REDIS_URL = url.parse(
   process.env.REDIS_URL || "redis://localhost:6379")
 
@@ -27,20 +30,30 @@ app.get('/', function (req, res) {
 });
 
 io.sockets.on('connection', function (socket) {
-  socket.emit('messages', 'hello world');
-
   console.log("redis: init")
+  cmd = "kato log drain add webdrain tcp://<this-domain>:" + TCP_PORT
+  socket.emit('messages', "Run: " + cmd)
   var redisConn = newRedisClient();
   redisConn.on("message", function(channel, message) {
     console.log("redis message " + channel + ": " + message)
     socket.emit('messages', message)
-  })
-  redisConn.on("subscribe", function(channel, count) {
-    console.log("redis: publishing on main")
-    redisMain.publish("drain", "sent via redis")
   })
   console.log("redis: subscribing..")
   redisConn.subscribe("drain")
 });
 
 console.log("Running at http://localhost:" + PORT)
+
+// TCP drain
+
+var tcpDrain = net.createServer(function (socket) {
+  var rl = readline.createInterface(socket, socket);
+  rl.prompt();
+  rl.on('line', function(line) {
+    console.log("Received: " + line)
+    redisMain.publish("drain", line)
+    rl.prompt();
+  })
+})
+
+tcpDrain.listen(TCP_PORT, '0.0.0.0')
